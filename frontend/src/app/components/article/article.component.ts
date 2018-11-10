8972c806b29ec0a2f3ea5df728d95032fd5e5be3
jakubbares <bares.jakub@gmail.com>
2018-11-10 16:51:51 +0100

    zmeny

--- frontend/src/app/components/article/article.component.ts
+++ frontend/src/app/components/article/article.component.ts
@@ -1,13 +1,20 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'sz-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.scss']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
   @Input() article: any;
+  @ViewChild('title') title;
+  @ViewChild('description') description;
   constructor() { }
+
+  ngOnInit() {
+    this.title.nativeElement.innerHTML = this.article.title;
+    this.description.nativeElement.innerHTML = this.article.description;
+  }
 
 
 }
